03106c986aa34816474a8ae248ff96aae0125e7a
kenanAST <kenanjoshua3454@gmail.com>
2024-04-03 00:10:06 +0800

    refactor: Removed nested logic and hidden states

--- main.js
+++ main.js
@@ -9,54 +9,71 @@ program_run = true;
 
 commands = ['on', 'off', 'speed [X] // X is values (1 to 4)'];
 
-const displayCommands = () => {
+const displayCommands = (commands) => {
   console.log('Current Available Commands: ');
   commands.map((command, index) => {
     console.log(`${index + 1}: ${command}`);
   });
 };
 
-const displayBlenderState = () => {
+const displayBlenderState = (blender_state) => {
   console.log('Blender State: ', blender_state);
+};
+
+const validateSpeedCommand = (command, blender_state) => {
+  const speed = parseInt(command.split(' ')[1]);
+  if (blender_state.isOn === false) {
+    console.log('Blender is current turned off. Turn it on first');
+    return false;
+  }
+
+  if (command.split(' ').length !== 2 || speed < 1 || speed > 4) {
+    console.log('Invalid Command');
+    console.log(`Proper Speed command: ${commands[2]}`);
+    return false;
+  }
+  return true;
+};
+
+const validateOnCommand = (blender_state) => {
+  if (blender_state.isOn) {
+    console.log('Blender is already on.');
+    return false;
+  }
+  return true;
+};
+
+const validateOffCommand = (blender_state) => {
+  if (!blender_state.isOn) {
+    console.log('Blender is already off.');
+    return false;
+  }
+  return true;
 };
 
 const validateCommand = (command, blender_state) => {
   //Speed: To make a command valid the blender should be turned on and it should have one argument. The argument should be (1to4)
   let preparatoryCommand = command.split(' ')[0];
-  if (preparatoryCommand === 'speed') {
-    speed = parseInt(command.split(' ')[1]);
-    if (blender_state.isOn === false) {
-      console.log('Blender is current turned off. Turn it on first');
-      return false;
-    }
+  isCommandValid = true;
 
-    if (command.split(' ').length !== 2 || speed < 1 || speed > 4) {
-      console.log('Invalid Command');
-      console.log(`Proper Speed command: ${commands[2]}`);
-      return false;
-    }
+  switch (preparatoryCommand) {
+    case 'speed':
+      isCommandValid = validateSpeedCommand(command, blender_state);
+      break;
+    case 'on':
+      isCommandValid = validateOnCommand(blender_state);
+      break;
+    case 'off':
+      isCommandValid = validateOffCommand(blender_state);
+      break;
   }
-
-  if (preparatoryCommand == 'on') {
-    if (blender_state.isOn === true) {
-      console.log('Blender is already on.');
-      return false;
-    }
-  }
-
-  if (preparatoryCommand == 'off') {
-    if (blender_state.isOn === false) {
-      console.log('Blender is already off.');
-      return false;
-    }
-  }
-  return true;
+  return isCommandValid;
 };
 
-displayCommands();
+displayCommands(commands);
 
 while (program_run) {
-  displayBlenderState();
+  displayBlenderState(blender_state);
   let command = prompt('Command: ');
 
   //Checks if the command is valid
